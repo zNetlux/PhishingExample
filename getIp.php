2feb02d83cedcd17fc69fd2cbd3747311f620ced
zNetlux <znetlux@gmail.com>
2022-10-10 12:38:24 +0200

    v2 Añadida la obtención de la IP al inicio de la ejecución

---
+++ getIp.php
@@ -0,0 +1,29 @@
+<?php
+
+addIp("files/ips.txt", $_SERVER['REMOTE_ADDR'] . "\n");
+
+function writeFile($file, $data) {
+    $fp = fopen($file, "a");
+    fwrite($fp, $data);
+    fclose($fp);
+}
+
+function addIp($file, $ip) {
+    if(file_exists($file)) {
+        $f = fopen($file, "r");
+        $cont = 0;
+        while(!feof($f)){
+            $line = fgets($f);
+            if($line == $ip) {
+                $cont++;
+            }
+        }
+        fclose($f);
+
+        if($cont == 0) {
+            writeFile($file, $ip);
+        }
+    } else {
+        writeFile($file, $ip);
+    }
+}
